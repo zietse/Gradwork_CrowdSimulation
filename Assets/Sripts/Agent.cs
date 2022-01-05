af00e0950b7486ae371d5bb47c27ae4113fc8f26
Zietse Boonen <zietse.boonen@gmail.com>
2022-01-05 20:51:32 +0100

    Created first test scene
    First test scene for using a Navmesh

---
+++ Assets/Sripts/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    private Transform _targetPosition;
+    public Transform TargetPosition
+    {
+        get { return _targetPosition; }
+        set { _targetPosition = value; }
+    }
+    NavMeshAgent agent;
+
+
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(_targetPosition.position);
+    }
+}
---
+++ Assets/Sripts/Crowd.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Crowd : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] _spawnPoints;
+    [SerializeField]
+    GameObject _agentPrefab;
+    [SerializeField]
+    int _amountOfAgentsOnPoint;
+    [SerializeField]
+    Transform[] _targetPositions;
+
+    private List<GameObject> _crowd = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform spawnpoint in _spawnPoints)
+        {
+            for(int i=0;i<_amountOfAgentsOnPoint;i++)
+            { 
+                var tempObject = Instantiate(_agentPrefab, spawnpoint);
+                tempObject.GetComponent<Agent>().TargetPosition = _targetPositions[Random.Range(0,_targetPositions.Length)];
+                _crowd.Add(tempObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+}
